c42ce0d7ad585cab4dbc2ef0fe3c62f0304d996a
你的名字 <email>
2017-01-18 13:59:58 +0800

    change some front

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :welcome do
+    
+  end
 
   resources :jobs do
     resources :resumes
   end
-  root 'jobs#index'
+  root 'welcome#index'
 
   namespace :admin do
     resources :jobs do
