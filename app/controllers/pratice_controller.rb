e4c579953fe40fba0a06a2e20c62562b95748189
你的名字 <email>
2017-01-22 09:37:41 +0800

    all down3

---
+++ app/controllers/pratice_controller.rb
@@ -0,0 +1,23 @@
+class PraticeController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @grou = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+end
